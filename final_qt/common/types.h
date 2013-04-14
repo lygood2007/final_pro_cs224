6b458a89ec1766b74821439789a7550a73e0b3b5
Yan Li <yanli@cs.brown.edu>
2013-04-14 15:48:39 -0400

    Added types

---
+++ final_qt/common/types.h
@@ -0,0 +1,12 @@
+/** types.h
+ ** Brief: This is the header file of all the necessary types in the project
+ ** Project: large-scale fluids
+ ** Date: 04/10/2013
+ ** Member: Scott, Hobarts, Yan Li
+ **/
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef float color4f[4];
+
+#endif // TYPES_H
