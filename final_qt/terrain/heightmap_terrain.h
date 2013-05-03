ab4eddfbf19873e4eb720001f7094a58adce30a9
hcreynol <hobart_reynolds@brown.edu>
2013-05-03 10:22:11 -0400

    anticipating merge

--- final_qt/terrain/heightmap_terrain.h
+++ final_qt/terrain/heightmap_terrain.h
@@ -10,7 +10,7 @@
 #include "terrain.h"
 #include <QFile>
 
-#define HEIGHTMAP_FILENAME "./resource/h_map_7.jpg"
+#define HEIGHTMAP_FILENAME "./resource/h_map_5.jpg"
 
 class HeightmapTerrain : public Terrain
 {
