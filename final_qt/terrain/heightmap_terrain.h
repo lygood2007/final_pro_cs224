e475f87cd095ca7dbe6d1e02d534a719592bf5f7
Scott Houde <shoude@cs.brown.edu>
2013-05-05 17:17:59 -0400

    tweaking the heightmap

--- final_qt/terrain/heightmap_terrain.h
+++ final_qt/terrain/heightmap_terrain.h
@@ -10,7 +10,7 @@
 #include "terrain.h"
 #include <QFile>
 
-#define HEIGHTMAP_FILENAME "./resource/h_map_7.jpg"
+#define HEIGHTMAP_FILENAME "./resource/h_map_8.jpg"
 
 class HeightmapTerrain : public Terrain
 {
